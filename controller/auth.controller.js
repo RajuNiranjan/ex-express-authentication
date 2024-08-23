d98538be864c792af7f2a3d3bfcc56d7294a7fc0
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-08-23 10:44:04 +0530

    created login route

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -24,3 +24,22 @@ export const Register = async (req, res, next) => {
         return res.status(500).json({ message: "Internal server error during registration" });
     }
 };
+
+export const LogIn = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await AuthModel.findOne({ email });
+        if (!user) return res.status(404).json({ message: "Invalid email address" });
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) return res.status(401).json({ message: "Invalid credentials" });
+
+        const token = generateToken(user._id);
+        res.setHeader('Authorization', `Bearer ${token}`);
+        return res.status(200).json({ message: "User logged in successfully" });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal server error during login" });
+    }
+}
--- routers/auth.router.js
+++ routers/auth.router.js
@@ -1,6 +1,7 @@
 import express from 'express'
-import { Register } from '../controller/auth.controller.js'
+import { LogIn, Register } from '../controller/auth.controller.js'
 
 export const AuthRouter = express.Router()
 
 AuthRouter.post('/register', Register)
+AuthRouter.post("/login", LogIn)
